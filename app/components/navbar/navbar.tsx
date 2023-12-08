c96de27201c1c5fc52fa0ce93b9878af5860a4b9
Luancss <ventobo123@gmail.com>
2023-12-08 13:01:32 -0300

    Hamburguer navigation

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../Container";
 import Logo from "./Logo"
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const navbar = () => {
   return (
@@ -16,6 +17,7 @@ const navbar = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo/>
             <Search/>
+            <UserMenu/>
             </div>
         </Container>
       </div>
