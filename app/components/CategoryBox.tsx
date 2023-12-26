cf1ff6f40deebd235644d24771c45865ffdb1aa5
Luancss <ventobo123@gmail.com>
2023-12-26 15:56:10 -0300

    Categories UI Complete

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,29 +1,64 @@
-"use client";
-import { useRouter, useSearchParams} from "next/navigation";
-import React, { useCallback } from "react";
+'use client';
+
+import qs from 'query-string';
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
-import qs from "query-string"
 
 interface CategoryBoxProps {
-  icon: IconType;
+  icon: IconType,
   label: string;
   selected?: boolean;
 }
 
-const CategoryBox: React.FC<CategoryBoxProps> = ({ icon: Icon, label, selected }) => {
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
   const router = useRouter();
   const params = useSearchParams();
 
   const handleClick = useCallback(() => {
-    let currentQuery = {}
-  }, [])
+    let currentQuery = {};
+    
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
 
-  return (
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
+
+    router.push(url);
+  }, [label, router, params]);
+
+  return ( 
     <div
-      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
-        selected ? "border-b-neutral-800" : "border-transparent"
-      }
-      ${selected ? "text-neutral-800" : "text-neutral-500"}
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
       `}
     >
       <Icon size={26} />
@@ -31,7 +66,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({ icon: Icon, label, selected }
         {label}
       </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default CategoryBox;
