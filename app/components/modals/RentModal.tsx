6bc8791a92195be2d926a2ce1b18a7be5840db56
Luancss <ventobo123@gmail.com>
2024-01-02 16:20:41 -0300

    Listing creation step

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,18 +1,80 @@
-'use client'
-import useRentModal from "@/app/hooks/useRentModal"
-import Modal from "./Modal"
+"use client";
+import useRentModal from "@/app/hooks/useRentModal";
+import Modal from "./Modal";
+import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div>
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vw] overflow-y-auto">
+        {categories.map((item) => (
+            <div key={item.label} className="col-span-1">
+              <CategoryInput
+                onClick={() => {}}
+                selected={false}
+                label={item.label}
+                icon={item.icon}
+              />
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <Modal
-    isOpen={rentModal.isOpen}
-    onClose={rentModal.onClose}
-    onSubmit={rentModal.onClose}
-    actionLabel="Submit"
-    title="Airbnb your home!"
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      actionLabel="Submit"
+      title="Airbnb your home!"
+      body={bodyContent}
     />
-  )
-}
+  );
+};
 
-export default RentModal
+export default RentModal;
