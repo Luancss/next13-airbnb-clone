8f14e0e421dc47222777adad480a2ae067ed9f5e
Luancss <ventobo123@gmail.com>
2024-01-04 23:57:56 -0300

    Implements react-select

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,8 +1,37 @@
 'use client'
 
-const CountrySelect = () => {
+import useCountries from "@/app/hooks/useContries";
+import Select from "react-select"
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+}
+
+interface CountrySelectProps {
+  value?: CountrySelectValue
+  onChange:(value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
+  value,
+  onChange
+}) => {
+  const {getAll} = useCountries();
+
   return (
-    <div>CountrySelect</div>
+    <div>
+      <Select
+      placeholder="Anywhere"
+      isClearable
+      options={getAll()}
+      value={value}
+      onChange={(value) => onChange(value as CountrySelectValue)}
+      />
+    </div>
   )
 }
 
