5e8508283450ae97889f717f8e20d409f5e113a2
Luancss <ventobo123@gmail.com>
2024-01-10 22:01:59 -0300

    Listing creation

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 interface CounterProps {
   title: string;
@@ -25,8 +26,32 @@ const Counter:React.FC<CounterProps> = ({
   }, [value, onChange])
 
   return (
-    <div>
-      
+    <div className='flex flex-row items-center justify-between'>
+      <div className='flex flex-col'>
+        <div className='font-medium'>
+           {title}
+        </div>
+        <div className='font-light text-gray-600'>
+          {subtitle}
+        </div>
+      </div>
+      <div className='flex flex-row items-center gap-4'>
+        <div
+        onClick={onReduce}
+        className='w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
+        >
+          <AiOutlineMinus/>
+        </div>
+        <div className='font-light text-lg text-neutral-600'>
+          {value}
+        </div>
+        <div
+        onClick={onAdd}
+        className='w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
+        >
+          <AiOutlinePlus/>
+        </div>
+      </div>
     </div>
   )
 }
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -50,6 +50,9 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -126,12 +129,31 @@ const RentModal = () => {
   
   if(step === STEPS.INFO) {
     bodyContent = (
-      <div>
+      <div className="flex flex-col gap-6">
         <Heading
         title="Share some basics about your place"
         subtitle="What amenities do you have?"
         />
-        <Counter/>
+        <Counter
+          title="Number of guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <hr/>
+         <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr/>
+         <Counter
+          title="Bathrooms"
+          subtitle="How many bathroms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
       </div>
     )
   }
