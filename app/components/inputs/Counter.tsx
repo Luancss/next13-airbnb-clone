8317c2081f4797224f66cbab34248700e1b6df28
Luancss <ventobo123@gmail.com>
2024-01-11 23:24:49 -0300

    Removing div for testing

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -36,12 +36,7 @@ const Counter:React.FC<CounterProps> = ({
         </div>
       </div>
       <div className='flex flex-row items-center gap-4'>
-        <div
-        onClick={onReduce}
-        className='w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition'
-        >
-          <AiOutlineMinus/>
-        </div>
+       
         <div className='font-light text-lg text-neutral-600'>
           {value}
         </div>
