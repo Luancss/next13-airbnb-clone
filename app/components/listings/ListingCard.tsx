f45c264fd7b82746f48e389b35fa9230f8c5ddd3
Luancss <ventobo123@gmail.com>
2024-01-26 23:39:25 -0300

    Add price on component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -104,6 +105,22 @@ const ListingCard: React.FC<ListingCardProps> = ({
         <div className="font-light text-neutral-500">
           {reservationDate || data.category}
         </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">
+            $ {price}
+          </div>
+          {!reservation && (
+            <div className="font-light">night</div>
+          )}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
