d0064ac2a0707b96907c7706f3baaa19b9bd5719
Luancss <ventobo123@gmail.com>
2024-01-30 15:26:53 -0300

    Create component ListingCategory

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+const ListingCategory = () => {
+  return (
+    <div>Listing Category</div>
+  )
+}
+
+export default ListingCategory
--- app/components/listings/ListingsInfo.tsx
+++ app/components/listings/ListingsInfo.tsx
@@ -1,15 +1,22 @@
-'use client'
+"use client";
 
-import { SafeUser } from "@/app/types"
+import useCountries from "@/app/hooks/useContries";
+import { SafeUser } from "@/app/types";
+import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
 
 interface ListingsInfoProps {
   user: SafeUser;
-  category: any
-  description: string
-  roomCount: number
-  guestCount: number
-  bathroomCount: number
-  locationValue: string
+  description: string;
+  roomCount: number;
+  guestCount: number;
+  bathroomCount: number;
+  category: {
+    icon: any;
+    label: string;
+    description: string;
+  };
+  locationValue: string;
 }
 
 const ListingsInfo: React.FC<ListingsInfoProps> = ({
@@ -19,11 +26,35 @@ const ListingsInfo: React.FC<ListingsInfoProps> = ({
   roomCount,
   guestCount,
   bathroomCount,
-  locationValue
+  locationValue,
 }) => {
-  return (
-    <div>Listings Info</div>
-  )
-}
+  const { getByValue } = useCountries();
 
-export default ListingsInfo
+  const coordinates = getByValue(locationValue)?.latlng;
+
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+          <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category?.label}
+          description={category?.description}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ListingsInfo;
