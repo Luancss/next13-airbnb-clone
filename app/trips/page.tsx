2c1fdfe1e0700cc2f6c41ccf6b6f07130991f50e
Luancss <ventobo123@gmail.com>
2024-02-02 14:51:57 -0300

    create page of trips

---
+++ app/trips/page.tsx
@@ -0,0 +1,44 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const TripsPage = async () => {
+
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      </ClientOnly>
+    )
+  }
+
+  const reservations = await getReservations({ userId: currentUser.id });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you haven't reserved any trips."
+        />
+      </ClientOnly>
+    )
+  }
+
+  return (
+    <ClientOnly>
+      <TripsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  )
+}
