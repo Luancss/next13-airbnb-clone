14a3af67774f6148eb5bb277a77a28aba0a91a75
Luancss <ventobo123@gmail.com>
2024-02-02 00:29:32 -0300

    Create reservation on mongodb

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -9,6 +9,7 @@ interface IParams {
 export default async function getReservations(
   params: IParams
 ) {
+  try {
     const { listingId, userId, authorId } = params;
 
     const query: any = {};
@@ -24,4 +25,30 @@ export default async function getReservations(
     if (authorId) {
         query.listing = { userId: authorId };
     }
+
+    const reservations = await prisma.reservation.findMany({
+        where: query,
+        include: {
+            listing: true
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const safeReservations = reservations.map((reservation) => ({
+        ...reservation,
+        createdAt: reservation.createdAt.toISOString(),
+        startDate: reservation.startDate.toISOString(),
+        endDate: reservation.endDate.toISOString(),
+        listing: {
+            ...reservation.listing,
+            createdAt: reservation.listing.createdAt.toISOString()
+        }
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
