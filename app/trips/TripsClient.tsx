58fa5417efea8cb07591da4421d09021235b3afb
Luancss <ventobo123@gmail.com>
2024-02-05 13:31:34 -0300

    Funtionality on delete trips id

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Container from "../components/Container";
 import Heading from "../components/Heading";
+import { useRouter } from "next/navigation";
 import { SafeReservation, SafeUser } from "../types";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 interface TripsClientProps {
   reservations: SafeReservation[];
@@ -9,16 +15,38 @@ interface TripsClientProps {
 
 const TripsClient: React.FC<TripsClientProps> = ({
   reservations,
-  currentUser
+  currentUser,
 }) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback((id: string) => {
+    setDeletingId(id);
+
+    axios
+      .delete(`/api/reservations/${id}`)
+      .then(() => {
+        toast.success("Reservation cancelled");
+      })
+      .catch((error) => {
+        toast.error(error?.response?.data?.error);
+      })
+      .finally(() => {
+        setDeletingId("");
+      })
+  }, []);
+
   return (
     <Container>
       <Heading
         title="Trips"
         subtitle="Where you've been and where you're going"
       />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default TripsClient
+export default TripsClient;
