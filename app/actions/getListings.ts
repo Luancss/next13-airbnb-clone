c7ef2cd7e3dfd4e03152e44a78ba8e6a14ebfabd
Luancss <ventobo123@gmail.com>
2024-02-06 15:44:15 -0300

    improving the functionality of parameters on the home

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,7 +1,22 @@
 import prisma from "@/app/libs/prismadb"
-export default async function getListings() {
+
+export interface IListingParams {
+  userId?: string;
+}
+export default async function getListings(
+  params: IListingParams
+) {
   try {
+    const { userId } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId
+    }
+    
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: 'desc'
       }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser()
 
   if (listings.length === 0) {
@@ -37,3 +41,5 @@ export default async function Home() {
     </ClientOnly>
   )
 }
+
+export default Home;
