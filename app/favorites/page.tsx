4ff2ca1832fa3b55ea60976961105b3441e318e4
Luancss <ventobo123@gmail.com>
2024-02-06 15:31:12 -0300

    Route structure

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,15 +2,28 @@ import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
 
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
 
 const ListingPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings"
+        />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
-      <EmptyState
-        title="No favorites found"
-        subtitle="Looks like you have no favorite listings"
-      />
-    </ClientOnly>  )
-}
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
 
-export default ListingPage
+export default ListingPage;
