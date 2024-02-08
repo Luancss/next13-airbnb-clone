896f6e2df36ec09c065a4f999c981582ea98af90
Luancss <ventobo123@gmail.com>
2024-02-08 02:34:23 -0300

    Conditionals for database

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -14,12 +14,47 @@ export default async function getListings(
   params: IListingParams
 ) {
   try {
-    const { userId } = params;
+    const { 
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+     } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    } 
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    } 
+
+    if (locationValue) {
+      query.locationValue = locationValue;
     }
     
     const listings = await prisma.listing.findMany({
