0c8f82ddb8f6ad611109d2bbacb5bc22517b5cdc
Luancss <ventobo123@gmail.com>
2024-02-08 12:44:18 -0300

    Fixing error

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,30 +1,31 @@
-"use client";
-import useCountries from "@/app/hooks/useContries";
-// @ts-nocheck
+'use client';
 
-import useSearchModal from "@/app/hooks/useSearchModal";
-import React, { useMemo } from "react";
-import { BiSearch } from "react-icons/bi";
-import { useSearchParams } from "next/navigation";
-import { differenceInDays } from "date-fns";
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { differenceInDays } from 'date-fns';
+
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useCountries from '@/app/hooks/useContries';
+
 
 const Search = () => {
   const searchModal = useSearchModal();
   const params = useSearchParams();
   const { getByValue } = useCountries();
 
-  const locationValue = params?.get("locationValue");
-  const startDate = params?.get("startDate");
-  const endDate = params?.get("endDate");
-  const guestCount = params?.get("guestCount");
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
 
   const locationLabel = useMemo(() => {
     if (locationValue) {
       return getByValue(locationValue as string)?.label;
     }
 
-    return "Anywhere";
-  }, [getByValue, locationValue]);
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
 
   const durationLabel = useMemo(() => {
     if (startDate && endDate) {
@@ -37,36 +38,92 @@ const Search = () => {
       }
 
       return `${diff} Days`;
-  }
-  }, [startDate, endDate])
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
 
   const guestLabel = useMemo(() => {
     if (guestCount) {
       return `${guestCount} Guests`;
     }
-    
-    return "Add Guests"
-  }, [guestCount])
 
-  return (
+    return 'Add Guests';
+  }, [guestCount]);
+
+  return ( 
     <div
       onClick={searchModal.onOpen}
-      className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
+      className="
+        border-[1px] 
+        w-full 
+        md:w-auto 
+        py-2 
+        rounded-full 
+        shadow-sm 
+        hover:shadow-md 
+        transition 
+        cursor-pointer
+      "
     >
-      <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">{locationLabel}</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+      <div 
+        className="
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+        "
+      >
+        <div 
+          className="
+            text-sm 
+            font-semibold 
+            px-6
+          "
+        >
+          {locationLabel}
+        </div>
+        <div 
+          className="
+            hidden 
+            sm:block 
+            text-sm 
+            font-semibold 
+            px-6 
+            border-x-[1px] 
+            flex-1 
+            text-center
+          "
+        >
           {durationLabel}
         </div>
-        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+        <div 
+          className="
+            text-sm 
+            pl-6 
+            pr-2 
+            text-gray-600 
+            flex 
+            flex-row 
+            items-center 
+            gap-3
+          "
+        >
           <div className="hidden sm:block">{guestLabel}</div>
-          <div className="p-2 bg-rose-500 rounded-full text-white">
+          <div 
+            className="
+              p-2 
+              bg-rose-500 
+              rounded-full 
+              text-white
+            "
+          >
             <BiSearch size={18} />
           </div>
         </div>
       </div>
     </div>
   );
-};
-
+}
+ 
 export default Search;
