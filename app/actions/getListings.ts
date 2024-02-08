7abcb3b5ce61a92891d8a421ce39b6b22f7e17a2
Luancss <ventobo123@gmail.com>
2024-02-08 02:35:45 -0300

    startDate and endDate sending condition to the database

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -56,6 +56,25 @@ export default async function getListings(
     if (locationValue) {
       query.locationValue = locationValue;
     }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
     
     const listings = await prisma.listing.findMany({
       where: query,
