cfbff0add89d2ea5def125fa2eb05b0f03ac3e0d
Luancss <ventobo123@gmail.com>
2024-02-08 02:30:43 -0300

    Params on component getListings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,6 +2,13 @@ import prisma from "@/app/libs/prismadb"
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 export default async function getListings(
   params: IListingParams
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,7 +1,9 @@
+'use client'
+// @ts-nocheck
+
 import useSearchModal from "@/app/hooks/useSearchModal";
 import React from "react";
-"use-client";
-import {BiSearch} from "react-icons/bi"
+import { BiSearch } from "react-icons/bi";
 
 const Search = () => {
   const searchModal = useSearchModal();
@@ -9,18 +11,17 @@ const Search = () => {
   return (
     <div
       onClick={searchModal.onOpen}
-     className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+      className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
+    >
       <div className="flex flex-row items-center justify-between">
         <div className="text-sm font-semibold px-6">Anywhere</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
           Any week
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">
-            Add Guests
-          </div>
+          <div className="hidden sm:block">Add Guests</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
-            <BiSearch size={18}/>
+            <BiSearch size={18} />
           </div>
         </div>
       </div>
