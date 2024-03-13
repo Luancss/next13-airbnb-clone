49007efca50adce653d0f18db1a7ff99ff9f1ddf
Luancss <ventobo123@gmail.com>
2024-03-13 14:02:52 -0300

    Deployment

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,18 +1,18 @@
-'use client';
 
-import EmptyState from "../components/EmptyState";
-import ClientOnly from "../components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
 
-import getCurrentUser from "../actions/getCurrentUser";
-import getReservations from "../actions/getReservations";
-import ReservationsClient from "./ReservationsClient";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
+
+import TripsClient from "./ReservationsClient";
 
 const ReservationsPage = async () => {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
     return (
-      <ClientOnly>
+      <ClientOnly> 
         <EmptyState
           title="Unauthorized"
           subtitle="Please login"
@@ -28,20 +28,20 @@ const ReservationsPage = async () => {
       <ClientOnly>
         <EmptyState
           title="No reservations found"
-          subtitle="Looks like you have no reservations on your properties"
+          subtitle="Looks like you have no reservations on your properties."
         />
       </ClientOnly>
-    )
+    );
   }
 
   return (
     <ClientOnly>
-      <ReservationsClient
+      <TripsClient
         reservations={reservations}
         currentUser={currentUser}
       />
     </ClientOnly>
-  )
+  );
 }
-
+ 
 export default ReservationsPage;
