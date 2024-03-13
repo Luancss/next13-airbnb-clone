125d4c40b0fac27428a72a2eb02bcf7c03ea5178
Luancss <ventobo123@gmail.com>
2024-03-13 15:34:31 -0300

    Otimizing project

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,47 +1,28 @@
-import { SafeUser } from "@/app/types";
 
-import Categories from "./Categories";
-import Container from "../Container";
-import Logo from "./Logo";
-import Search from "./Search";
-import UserMenu from "./UserMenu";
+import { SafeUser } from '@/app/types';
+import Categories from './Categories';
+import UserMenu from './UserMenu';
+import Search from './Search';
+import Container from '../Container';
+import Logo from './Logo';
+
 
 interface NavbarProps {
-  currentUser?: SafeUser | null;
+	currentUser?: SafeUser | null;
 }
-
-const Navbar: React.FC<NavbarProps> = ({
-  currentUser,
-}) => {
-  return ( 
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div
-        className="
-          py-4 
-          border-b-[1px]
-        "
-      >
-      <Container>
-        <div 
-          className="
-            flex 
-            flex-row 
-            items-center 
-            justify-between
-            gap-3
-            md:gap-0
-          "
-        >
-          <Logo />
-          <Search />
-          <UserMenu currentUser={currentUser} />
-        </div>
-      </Container>
-    </div>
-    <Categories />
-  </div>
-  );
-}
-
-
-export default Navbar;
+export const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+	return (
+		<div className='fixed w-full bg-white z-10 shadow-sm'>
+			<div className='py-4 border-b'>
+				<Container>
+					<div className='flex flex-row items-center justify-between gap-4 md:gap-0'>
+						<Logo />
+						<Search />
+						<UserMenu currentUser={currentUser} />
+					</div>
+				</Container>
+			</div>
+			<Categories />
+		</div>
+	);
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,42 @@
-import { Nunito } from 'next/font/google'
+import './globals.css';
+import { Nunito } from 'next/font/google';
 
+import { Toaster } from 'react-hot-toast';
 
-import LoginModal from '@/app/components/modals/LoginModal';
-import RegisterModal from '@/app/components/modals/RegisterModal';
-import SearchModal from '@/app/components/modals/SearchModal';
-import RentModal from '@/app/components/modals/RentModal';
-
-
-import './globals.css'
-import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
-import ToasterProviders from './providers/ToasterProviders';
-import Navbar from './components/navbar/Navbar';
+import RentModal from './components/modals/RentModal';
+import LoginModal from './components/modals/LoginModal';
+import RegisterModal from './components/modals/RegisterModal';
+import SearchModal from './components/modals/SearchModal';
+import { Navbar } from './components/navbar/Navbar';
+
+
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+	title: 'Airbnb',
+	description: 'Airbnb clone',
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+	subsets: ['latin'],
 });
-
 export default async function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser();
-
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ClientOnly>
-          <ToasterProviders />
-          <LoginModal />
-          <RegisterModal />
-          <SearchModal />
-          <RentModal />
-          <Navbar currentUser={currentUser} />
-        </ClientOnly>
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
-      </body>
-    </html>
-  )
+	const currentUser = await getCurrentUser();
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<Toaster />
+				<RentModal />
+				<LoginModal />
+				<RegisterModal />
+				<SearchModal />
+				<Navbar currentUser={currentUser} />
+				<div className='pb-20 pt-28'>{children}</div>
+			</body>
+		</html>
+	);
 }
